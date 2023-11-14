3d19a531ace22ddbbb3666c8bf9b357eec62e61e
Yaerin Park <rnrmf941@gmail.com>
2023-11-14 23:22:11 +0900

    :see_no_evil: Chore: .gitignore 적용

--- node_modules/iron-session/koa/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { IronSessionOptions } from 'iron-session';
-import * as Koa from 'koa';
-
-declare function ironSession(sessionOptions: IronSessionOptions): (ctx: Koa.Context, next: Koa.Next) => Promise<void>;
-
-export { ironSession };
